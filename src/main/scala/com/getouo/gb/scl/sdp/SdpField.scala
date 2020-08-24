dbb86db6d02110c8fc1139ae0b00e7b3e83e649a
getouo <soliangsx@hotmail.com>
2020-08-25 01:14:23 +0800

    md

--- src/main/scala/com/getouo/gb/scl/sdp/SdpField.scala
+++ src/main/scala/com/getouo/gb/scl/sdp/SdpField.scala
@@ -1,5 +1,21 @@
 package com.getouo.gb.scl.sdp
 
+case class SdpFieldName(name: Char) extends AnyVal
+
+object SdpFieldName {
+  val VERSION = SdpFieldName('v')
+  val ORIGIN = SdpFieldName('o')
+  val SESSION_NAME = SdpFieldName('s')
+  val CONNECTION = SdpFieldName('c')
+  val BANDWIDTH = SdpFieldName('b')
+  val TIMES = SdpFieldName('t')
+
+  val ATTR = SdpFieldName('a')
+
+  val M_MEDIA = SdpFieldName('m')
+
+}
+
 trait SdpField[V <: Any] {
   val key: String
   val value: String
